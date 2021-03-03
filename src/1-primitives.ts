1e218cd8c16cf8073786d18c47ad1021b21ea0a7
GodLuckMister <paet14@gmail.com>
2021-03-04 00:13:48 +0200

    add new file

---
+++ src/1-primitives.ts
@@ -0,0 +1,6 @@
+export { }
+
+let age: number[] = [1, 2, 3, 4, 5];
+
+
+console.log(age);
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import './1-primitives';
+// import './2-arrays';
+// import './3-types';
+// import './4-interface';
+// import './5-enums';
+// import './6-functions';
+// import './7-classes';
+export { }
